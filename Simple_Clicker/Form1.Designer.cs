5ad386b94bb6a43db5d3d75a15d75d3a33278aef
Nadalika <kuberska.aneta@gmail.com>
2024-01-14 16:01:36 +0100

    Dodanie przycisków, ustawienie możliwości jednorazowego zakupu. Dodanie assetów.

--- Simple_Clicker/Form1.Designer.cs
+++ Simple_Clicker/Form1.Designer.cs
@@ -28,13 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.waluta_label = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // waluta_label
+            // 
+            this.waluta_label.AutoSize = true;
+            this.waluta_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.waluta_label.Location = new System.Drawing.Point(382, 91);
+            this.waluta_label.Name = "waluta_label";
+            this.waluta_label.Size = new System.Drawing.Size(53, 20);
+            this.waluta_label.TabIndex = 1;
+            this.waluta_label.Text = "label1";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Simple_Clicker.Properties.Resources.White_crystal1;
+            this.pictureBox5.Location = new System.Drawing.Point(627, 384);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Simple_Clicker.Properties.Resources.Violet_crystal1;
+            this.pictureBox6.Location = new System.Drawing.Point(627, 218);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::Simple_Clicker.Properties.Resources.Black_crystal1;
+            this.pictureBox7.Location = new System.Drawing.Point(627, 38);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 5;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Simple_Clicker.Properties.Resources.Pink_crystal1;
+            this.pictureBox4.Location = new System.Drawing.Point(50, 384);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Simple_Clicker.Properties.Resources.Red_crystal1;
+            this.pictureBox3.Location = new System.Drawing.Point(50, 218);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Simple_Clicker.Properties.Resources.Yellow_crystal1;
+            this.pictureBox2.Location = new System.Drawing.Point(50, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Simple_Clicker.Properties.Resources._68308;
+            this.pictureBox1.Location = new System.Drawing.Point(314, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.waluta_label);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label waluta_label;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
     }
 }
 
--- Simple_Clicker/Form1.cs
+++ Simple_Clicker/Form1.cs
@@ -12,9 +12,146 @@ namespace Simple_Clicker
 {
     public partial class Form1 : Form
     {
+
+        int waluta = 0;
+        int ilosc = 1;
+        public bool pictureBox2Bought = false;
+        public bool pictureBox3Bought = false;
+        public bool pictureBox4Bought = false;
+        public bool pictureBox7Bought = false;
+        public bool pictureBox6Bought = false;
+        public bool pictureBox5Bought = false;
+
         public Form1()
         {
             InitializeComponent();
+            waluta_label.Text = string.Empty;
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            waluta += ilosc;
+            waluta_label.Text = waluta.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2Bought)
+            {
+                MessageBox.Show("Nie można dokonać zakupu!", "OKNO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+            else if (!pictureBox2Bought)
+            {
+                if (waluta >= 10)
+                {
+                    ilosc = 5;
+                    waluta -= 10;
+                    waluta_label.Text = waluta.ToString();
+                }
+                pictureBox2Bought = true;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox3Bought)
+            {
+                MessageBox.Show("Nie można dokonać zakupu!", "OKNO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (!pictureBox3Bought)
+            {
+                if (waluta >= 10)
+                {
+                    ilosc = 5;
+                    waluta -= 10;
+                    waluta_label.Text = waluta.ToString();
+                }
+                pictureBox3Bought = true;
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox4Bought)
+            {
+                MessageBox.Show("Nie można dokonać zakupu!", "OKNO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (!pictureBox4Bought)
+            {
+                if (waluta >= 10)
+                {
+                    ilosc = 5;
+                    waluta -= 10;
+                    waluta_label.Text = waluta.ToString();
+                }
+                pictureBox4Bought = true;
+            }
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            if (pictureBox7Bought)
+            {
+                MessageBox.Show("Nie można dokonać zakupu!", "OKNO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (!pictureBox7Bought)
+            {
+                if (waluta >= 10)
+                {
+                    ilosc = 5;
+                    waluta -= 10;
+                    waluta_label.Text = waluta.ToString();
+                }
+                pictureBox7Bought = true;
+            }
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            if (pictureBox6Bought)
+            {
+                MessageBox.Show("Nie można dokonać zakupu!", "OKNO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (!pictureBox6Bought)
+            {
+                if (waluta >= 10)
+                {
+                    ilosc = 5;
+                    waluta -= 10;
+                    waluta_label.Text = waluta.ToString();
+                }
+                pictureBox6Bought = true;
+            }
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox5Bought)
+            {
+                MessageBox.Show("Nie można dokonać zakupu!", "OKNO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (!pictureBox5Bought)
+            {
+                if (waluta >= 10)
+                {
+                    ilosc = 5;
+                    waluta -= 10;
+                    waluta_label.Text = waluta.ToString();
+                }
+                pictureBox5Bought = true;
+            }
         }
     }
 }
